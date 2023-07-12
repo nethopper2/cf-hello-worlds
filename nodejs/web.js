4f60c42b44f5a931102dbc11923760fddf3f44e1
cmunford73 <chris@nethopper.io>
2023-07-12 18:52:08 -0400

    committing manifest file ./kube/hello-world.yaml version 0.0.2

--- nodejs/web.js
+++ nodejs/web.js
@@ -3,6 +3,7 @@ const os = require('os');
 const port = process.env.PORT || 5000;
 
 const runTimeEnv = process.env.RUNTIMEENV || "CloudFoundry";
+const version = process.env.VERSION || "0.0.1";
 
 http.createServer( (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
